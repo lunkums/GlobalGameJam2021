3b535decfb28fa402cdb9fb6cd7a1182f339b280
Luke Leedy <lukeleedy123@gmail.com>
2021-01-28 23:05:36 -0500

    Updated the movement to make it smoother on downward slopes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,9 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
-    
+    public float slopeForce = 25f;
+    public float slopeForceRayLength = 2.6f;
+
     public float minJumpHeight = 2f;
     public float maxJumpHeight = 10f;
     /*public float minJumpDistance = 2f;
@@ -23,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 velocity;
     private bool isGrounded;
     private bool isJumping;
+    private bool isOnSlope;
     private float jumpHeight = 0f;
 
     // Start is called before the first frame update
@@ -34,7 +37,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isOnSlope = OnSlope();
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask) || isOnSlope;
 
         // Jump behavior
         if (isGrounded)
@@ -65,32 +69,51 @@ public class PlayerMovement : MonoBehaviour
                 {
                     jumpHeight += minJumpHeight;
                     velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-                    velocity.x = jumpDistance * transform.forward.x;
-                    velocity.z = jumpDistance * transform.forward.z;
                     jumpHeight = 0f;
-                }
-                else
-                {
-                    velocity.x = 0;
-                    velocity.z = 0;
                 }
             }
             // Player must be jumping if jumpHeight or y velocity is greater than 0
             isJumping = jumpHeight > 0f || velocity.y > 0;
         }
 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
         // Apply horizontal and vertical motion
+        Vector3 move = Vector3.zero;
         if (!isJumping)
         {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-
-            Vector3 move = transform.right * x + transform.forward * z;
-            controller.Move(move * speed * Time.deltaTime);
+            move += transform.right * x + transform.forward * z;
         }
+        else if (!isGrounded)
+        {
+            move += transform.forward;
+        }
+        controller.Move(move * speed * Time.deltaTime);
 
         // Apply gravity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        // Apply additional downward force if on slope
+        if ((x != 0 || z != 0) && isOnSlope)
+        {
+            controller.Move(Vector3.down * controller.height / 2 * slopeForce * Time.deltaTime);
+        }
+    }
+
+    // Uses raycasting to determine if the player is on a slope
+    private bool OnSlope()
+    {
+        bool isOnSlope = false;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, controller.height / 2.0f * slopeForceRayLength))
+        {
+            if (hit.normal != Vector3.up)
+            {
+                isOnSlope = true;
+            }
+        }
+        return isOnSlope;
     }
 }
